d766c6d75a2303e95f0674d0ce58c9b97e5ab7d7
Leo_Sandler <leo.sandler@greenwoodcollege.com>
2018-12-20 14:03:53 -0500

    Added some functionality to my code.

--- SR_Main.py
+++ SR_Main.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr  # This import allows for the function to be used multiple times, condesing the amount of lines.
 import webbrowser as wb
-import keyboard as k
+import time
+from weather import *
 
 
 def speech():
@@ -8,27 +9,80 @@ def speech():
 
     with sr.Microphone() as source:  # This enables the microphone to be used.
         r.adjust_for_ambient_noise(source)
-        print("Say Something: ")
-        audio = r.listen(source)  # Listening to the audio source and saving it under the variable source.
+        timelimit = input(int("How long will you be speaking for: "))
+        print("Speak Now: ")
+        audio = r.listen(source, phrase_time_limit=timelimit)  # Listening to the audio source and saving it under the variable source.
         try:  # Used to verify that the audio is clear
-            text = r.recognize_google(audio)
-            print("You said: " + str(text))
-        except:
+            dictation = r.recognize_google(audio)
+            return dictation
+        except sr.UnknownValueError:
             print("Could not recognize that input")
 
 
+def message_recorder():
+    filename = input(str("File Title: "))
+    user_file = open(filename, "w")
+    user_file.write(speech())
+# Can append to the files, via opening old ones. This should also have a menu within it.
+
+
 def search():
-    inquiry = input("What do you want to search on Google: ")
-    wb.open_new_tab('http://google.com/?q=%s' % inquiry)  # the % appends the string onto the search
-    g = ("http://groodle.greenwoodcollege.com/")
+    wb.open_new_tab('https://www.google.com/search?q=%s' % speech())  # the % appends the string onto the search
 
 
-speech()
+def date():
+    print("Enter Your Location: ")
+    user_location = (speech().title())
+    localtime = time.asctime(time.localtime(time.time()))
+    print("Today's Date and Your Local Time: " + localtime)
+    weather = Weather(unit=Unit.CELSIUS)
+    location = weather.lookup_by_location(user_location)
+    forecasts = location.forecast
+    print("Note: All temperatures are in Celsius")
+    for forecast in forecasts:
+        print("On " + forecast.date + ", in " + user_location + " the forecast calls for " + forecast.text +
+              ". The high will be " + forecast.high + ". The low will be " + forecast.low + ".")
 
+
+def spelling():
+    print("Spelling")
+
+
+def restart():
+    restart_q = input("(Y/N) Do you want to start again? ").lower()
+    if restart_q == "y":
+        menu()
+    else:
+        print("Thanks for using Leo's speech recognition application.")
+        exit()
+
+
+
+def menu():
+    selection = input("Options:\n"
+                      "A) Message Recorder\n"
+                      "B) Spelling Bee\n"
+                      "C) Bing Search\n"
+                      "D) Time of Day and Weather\n"
+                      "Your Choice: ").upper()
+    if selection == "A":
+        message_recorder()
+    elif selection == "B":
+        spelling()
+    elif selection == "C":
+        search()
+    elif selection == "D":
+        date()
+    else:
+        print("You selected an invalid input.")
+        restart()
+
+
+menu()
 
 # COULD BE USED
-# phrase_time_limit = input(int("How long will you be speaking for: "))
+
 # x = k.wait('esc')
 # k.wait(hotkey=None, suppress=False, trigger_on_release=False)
 # Learn how to change languages
-# Time of day
+# Spelling Bee
